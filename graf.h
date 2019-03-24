99eaf9d270a43a75044ce12078143eb981c07427
CosminBirjoveanu <48485136+CosminBirjoveanu@users.noreply.github.com>
2019-03-24 23:29:13 +0200

    Update graf.h

--- graf.h
+++ graf.h
@@ -1,29 +1,30 @@
 #include "lista.h"
 
 class Graf{
-    int varf, nr_muchii;
-    Lista *array;
-    void verifica(int rad, int visited[]);
+    int varf, nr_muchii;                    //varf = numarul de varfuri
+    Lista *array;                           //vector de liste de adiacenta
+    void verifica(int, int[]);              //functie recursiva folosita de DFS pt a verifica daca un varf a fost vizitat
+    void verificaConex(int, int[]);
+    int verifCiclic(int, int[], int);       //verifica daca graful este ciclic
 public:
     Graf();
     Graf(int);
-    Graf(const Graf&);
+    Graf(const Graf&);                      //constructor de copiere
     ~Graf();
     void setNrVarfuri(int);
     void setNrMuchii(int);
     int getNrVarfuri();
     int getNrMuchii();
-    int getSize(int);
-    void adVarf();
+    int getSize(int);                       //in cazul in care graful a fost initializat prin constructorul
+    void adVarf();                          //fara parametri aceasta functie ii adauga un varf
     void adMuchie(int , int);
-    void compConexe();
+    int compConexe();                      //afiseaza pe ecran numarul de componente conexe
     void BFS(int);
-    int dist(int, int);
+    int dist(int, int);                     //intoarce distanta minima dintre 2 varfuri date
     void DFS(int rad);
-    int verifCiclic(int, int[], int);
     int eArbore();
     int* operator[](int);
-    friend int operator< (Graf, Graf);
+    friend int operator< (Graf, Graf);      //functiile prieten usureaza utilizarea parametrilor privati din clasa
     friend int operator> (Graf, Graf);
     friend std::istream &operator>> (std::istream &, Graf &);
     friend std::ostream &operator<< (std::ostream &, Graf&);
